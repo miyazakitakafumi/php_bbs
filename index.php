09f2bee7982720b912d4bece677bbbe7b117e7b8
Takaｆumi Miyazaki <Takaumi Miyazaki>
2016-09-25 01:13:25 +0900

    index.php added

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <title>PHP BBS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>BBS Test</h1>
+    </body>
+</html>
